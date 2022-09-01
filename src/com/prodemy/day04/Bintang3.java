93c275d7cdc7ebb2756f17358e7084a802ddb306
rizkiabdillahazmi <rizkiabdillahazmi10@gmail.com>
2022-09-01 16:20:54 +0700

    Fibonacci Challenge

--- src/com/prodemy/day04/Bintang3.java
+++ src/com/prodemy/day04/Bintang3.java
@@ -1,12 +1,30 @@
 package com.prodemy.day04;
 
+import java.util.Scanner;
+
 public class Bintang3 {
     public static void main(String[] args) {
+        System.out.print("Masukkan Bilangan : ");
+        Scanner sc = new Scanner(System.in);
+        bilangan(sc.nextInt());
+    }
+
+    private static void bintang() {
         int n = 9;
-        for (int i = 0; i < n; i++) {
-            for (int j = n; j >= 0; j--) {
-                if (i==j) {
-                    System.out.print("*");
+        for (int baris = 0; baris < n; baris++) {
+            for (int kolom = 0; kolom <= n; kolom++) {
+                String cetak = (baris==kolom) ? "*":"  ";
+                System.out.print(cetak);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void bilangan(int n) {
+        for (int baris = 1; baris <= n; baris++) {
+            for (int kolom = 1; kolom <= n; kolom++) {
+                if (baris==kolom) {
+                    System.out.print(baris);
                 } else {
                     System.out.print("  ");
                 }
---
+++ src/com/prodemy/day04/Bintang4.java
@@ -0,0 +1,66 @@
+package com.prodemy.day04;
+
+import java.util.Scanner;
+
+import com.prodemy.util.StringUtil;
+
+public class Bintang4 {
+    public static void main(String[] args) {
+        System.out.print("Masukkan bilangan : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // if (n % 2 == 0) {
+        //     System.out.println("Anda Memasukkan Bilangan Genap !!!");
+        // } else {
+        //     bintang(n);
+        //     cetakBilanganGanjil(n);
+        // }
+        cetakFibonacci(n);
+    }
+
+    private static void bintang(int n) {
+        for (int baris = 1; baris <= n; baris++) {
+            for (int kolom = 1; kolom <= n; kolom++) {
+                String cetak = (baris == kolom || baris == n - kolom + 1) ? "* " : "  ";
+                System.out.print(cetak);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void cetakBilangan(int n) {
+        for (int baris = 1; baris <= n; baris++) {
+            for (int kolom = 1; kolom <= n; kolom++) {
+                String cetak = (baris == kolom || baris == n - kolom + 1) ? String.valueOf(kolom) + " " : "  ";
+                System.out.print(cetak);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void cetakBilanganGanjil(int n) {
+        for (int baris = 1; baris <= n; baris++) {
+            for (int kolom = 1; kolom <= n; kolom++) {
+                String cetak = (baris==kolom || baris==n-kolom+1) ? String.valueOf((kolom-1)*2+1)+" " : "  ";
+                System.out.print(cetak);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void cetakFibonacci(int n) {
+        int[] bilangan = new int[n];
+        for (int i = 0; i < n; i++) {
+            int fibonacci = (i < 2) ?  1 : bilangan[i-1]+bilangan[i-2];
+            bilangan[i] = fibonacci;
+        }
+
+        for (int baris = 1; baris <= n; baris++) {
+            for (int kolom = 1; kolom <= n; kolom++) {
+                String cetak = (baris==kolom || baris==n-kolom+1) ? String.valueOf(bilangan[kolom-1])+" " : "  ";
+                System.out.print(StringUtil.pad(cetak, 3));
+            }
+            System.out.println();
+        }
+    }
+}
